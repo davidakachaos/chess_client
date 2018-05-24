ac7f53eb5ce34fab74b113f598716f3f05194f55
David Westerink <davidakachaos@gmail.com>
2018-05-24 12:34:37 +0200

    When a new game is lined up, go to it
    Also when dequeuing, don't request a new place in line to wait

--- queue_for_game.py
+++ queue_for_game.py
@@ -2,7 +2,7 @@
 from kivy.uix.screenmanager import Screen
 from kivy.app import App
 from kivy.logger import Logger
-from kivy.clock import Clock, mainthread
+from kivy.clock import mainthread
 from kivy.uix.screenmanager import ScreenManager
 from kivy.properties import ObjectProperty
 from models.player import Player
@@ -31,17 +31,22 @@ class QueueForGame(Screen):
         while new_game_id == "queued_for_game":
             tim = datetime.now()
             seconds_in_q = int((tim - start_q).total_seconds())
-
+            self.update_label_text(f"In de wachtrij voor een nieuw spel.\nTijd in wachtrij: {seconds_in_q}")
+            sleep(1)
             if self.stop.is_set():
                 # Need to de-queue?
                 self.update_label_text(f"Wachtrij verlaten!\nTijd in wachtrij: {seconds_in_q}")
                 return
-            self.update_label_text(f"In de wachtrij voor een nieuw spel.\nTijd in wachtrij: {seconds_in_q}")
-            sleep(1)
-            new_game_id = app.player.net.queue_up_new_game(blocking=False)
+            else:
+                new_game_id = app.player.net.queue_up_new_game(blocking=False)
 
         game = Game(new_game_id, app.player.net)
         self.update_label_text(f"Nieuw spel tegen {game.my_opponent} gestart!")
+        sleep(2)
+        game_screen = app.manager.get_screen('game')
+        game_screen.game_instance = game
+        app.manager.current = 'game'
+        game_screen.load_game()
 
     def dequeue(self):
         app = App.get_running_app()
