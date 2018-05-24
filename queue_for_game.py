f3e4d38fabf3349ceba37469c2cf55279af3401e
David Westerink <davidakachaos@gmail.com>
2018-05-24 11:44:05 +0200

    Queue for new game

--- queue_for_game.py
+++ queue_for_game.py
@@ -1,12 +1,15 @@
 """Manages the Screen for queuing for games."""
 from kivy.uix.screenmanager import Screen
 from kivy.app import App
+from kivy.logger import Logger
 from kivy.clock import Clock, mainthread
 from kivy.uix.screenmanager import ScreenManager
 from kivy.properties import ObjectProperty
 from models.player import Player
 from models.game import Game
 import threading
+from datetime import datetime
+from time import sleep
 
 
 class QueueForGame(Screen):
@@ -14,20 +17,45 @@ class QueueForGame(Screen):
 
     stop = threading.Event()
 
-    def start_queue_up(self):
+    def start_queue_up(self, *args):
+        """Spawns a thread to queue for a new game."""
+        # Reset the flag if needed.
+        self.stop.clear()
         threading.Thread(target=self.queue_up).start()
 
     def queue_up(self):
         app = App.get_running_app()
-        try:
-            self.update_label_text("In de wachtrij voor een nieuw spel")
-        except Exception as e:
-            return False
+        self.update_label_text("In de wachtrij voor een nieuw spel")
+        start_q = datetime.now()
+        new_game_id = app.player.net.queue_up_new_game(blocking=False)
+        while new_game_id == "queued_for_game":
+            tim = datetime.now()
+            seconds_in_q = int((tim - start_q).total_seconds())
 
-        # This blocks the thread!!
-        new_game_id = app.player.net.queue_up_new_game()
+            if self.stop.is_set():
+                # Need to de-queue?
+                self.update_label_text(f"Wachtrij verlaten!\nTijd in wachtrij: {seconds_in_q}")
+                return
+            self.update_label_text(f"In de wachtrij voor een nieuw spel.\nTijd in wachtrij: {seconds_in_q}")
+            sleep(1)
+            new_game_id = app.player.net.queue_up_new_game(blocking=False)
+
         game = Game(new_game_id, app.player.net)
         self.update_label_text(f"Nieuw spel tegen {game.my_opponent} gestart!")
+
+    def dequeue(self):
+        app = App.get_running_app()
+        self.stop.set()
+        dequeue_res = app.player.net.dequeue()
+        if dequeue_res == "dequeued":
+            Logger.info("Successvol uit wachtrij gegaan.")
+        else:
+            Logger.info("Stond niet in wachtrij!")
+
+        # Switch back to the overview screen.
+        sleep(2)
+        app.manager.current = "overview"
+
 
     @mainthread
     def update_label_text(self, new_text):
