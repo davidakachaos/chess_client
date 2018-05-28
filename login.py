abf113d0e681d985f779100df47f529a483e3f3b
David Westerink <davidakachaos@gmail.com>
2018-05-28 10:41:52 +0200

    Added documentation etc.

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+"""The login screen module."""
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen, FallOutTransition
@@ -11,12 +12,21 @@ from models.player import Player
 
 
 class Login(Screen):
+    """The login screen.
 
-    def __init__(self, name=None, *args):
+    Parameters
+    ----------
+    name : String
+        The name of this screen as the screen manager can access.
+
+    """
+
+    def __init__(self, name=None, *args):  # noqa: D107
         super(Login, self).__init__(name=name, *args)
         Clock.schedule_once(self._setup_boxes, 0)
 
     def _setup_boxes(self, _):
+        """Set the textboxes properties on load."""
         Logger.debug('Setting boxes')
         self.ids.login.write_tab = False
         self.ids.login.focus = True
@@ -25,10 +35,26 @@ class Login(Screen):
         self.ids.password.bind(on_text_validate=self.on_enter)
 
     def on_enter(self, *instance):
+        """Login when user hits enter in a textbox."""
         if instance:
             self.do_login(self.ids['login'].text, self.ids['password'].text)
 
     def do_login(self, loginText, passwordText):
+        """Login to the server with login and password.
+
+        Parameters
+        ----------
+        loginText : String
+            The username to login with.
+        passwordText : String
+            The unhashed password to try the login with.
+
+        Returns
+        -------
+        If login is sucessful, proceed to the application.
+        Else show a popup and clear the textboxes.
+
+        """
         app = App.get_running_app()
 
         app.username = loginText
@@ -54,10 +80,12 @@ class Login(Screen):
             self.loginFailedMessage()
 
     def goto_register(self):
+        """Change to the register account screen."""
         self.manager.transition = FallOutTransition()
         self.manager.current = "registration"
 
     def loginFailedMessage(self):
+        """Show a login failed message to the user."""
         layout = GridLayout(cols=1, padding=10)
         popupLabel = Label(text="Login failed!")
         closeButton = Button(text="Okay")
@@ -74,8 +102,10 @@ class Login(Screen):
         closeButton.bind(on_press=popup.dismiss)
 
     def refocus(self, _):
+        """Schedual a call to refocus the login textbox."""
         Clock.schedule_once(self._setup_boxes, 0.3)
 
     def resetForm(self):
+        """Reset the textboxes to empty."""
         self.ids['login'].text = ""
         self.ids['password'].text = ""
